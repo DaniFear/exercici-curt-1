d59eb2c7736bce5e5abcdacb91d864ea041c8828
DaniFear <97611343+DaniFear@users.noreply.github.com>
2022-02-11 16:15:26 +0100

    mitad hecho

--- jdk/Program.cs
+++ jdk/Program.cs
@@ -4,9 +4,47 @@ namespace jdk
 {
     internal class Program
     {
+        private static string[] opcions = new string[] { "PEDRA", "PAPER", "TISORA" };
+
+        private static string[,] resultats = new string[,] {{"Empat", "Perds", "Guanyes"},
+                                                            {"Guanyes", "Empat", "Perds"},
+                                                            {"Perds", "Guanyes", "Empat"}};
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        private static string comprovarQuiGuanya(int opcioUsuari, int opcioOrdinador)
+        {
+            Console.WriteLine("      Usuari: " + opcioUsuari);
+            Console.WriteLine("      Ordinador: " + opcioOrdinador);
+            Console.WriteLine("      " + resultats[opcioUsuari, opcioOrdinador]);
+
+            return resultats[opcioUsuari, opcioOrdinador];
+        }
+
+        private static int opcioUsuari()
+        {
+            string opcio;
+            int number;
+
+            Console.WriteLine("Selecciona una opció (1, 2 o 3):");
+            for(int i = 0; i < opcions.Length; i++)
+            {
+                Console.WriteLine(i+1 + "->" + opcions[i]);
+            }
+            opcio = Console.ReadLine();
+            bool success = int.TryParse(opcio, out number);
+            if (success || (number>=1 && number<=3))
+            {
+               return number-1;
+            }
+            else
+            {
+                return opcioUsuari();
+            }
+
         }
     }
 }
